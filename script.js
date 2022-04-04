361689cd2502dd9bec014bb4b34ddec387cfd07d
Advaithva <97515865+Advaithva@users.noreply.github.com>
2022-04-04 16:24:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function BLR() {
-	window.alert("Train no. - 22692 Hazrat Nizamuddin Rajdhani Express")
+	window.alert("Train no. - 22692 Hazrat Nizamuddin Bengaluru Rajdhani Express")
 	window.alert("Location - Hazrat Nizamuddin Railway Station")
 }
