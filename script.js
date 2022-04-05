b6d627fdc458f2c92e087d0a5fa8c95f7a4799f0
Advaithva <97515865+Advaithva@users.noreply.github.com>
2022-04-05 13:42:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function BLR() {
-	window.alert("Train no. - 22692 Hazrat Nizamuddin Bengaluru Rajdhani Express")
-	window.alert("Location - Hazrat Nizamuddin Railway Station")
+	alert("Train no. - 22692 Hazrat Nizamuddin Bengaluru Rajdhani Express")
+	alert("Location - Hazrat Nizamuddin Railway Station")
 }
 function INT() {
-	window-alert("Train no. 12678 Ernakulam Bengaluru Intercity Express")
+	alert("Train no. 12678 Ernakulam Bengaluru Intercity Express")
 
 } 
 function PASS() {
-	window.alert("Train no. 16128 Guruvayur Chennai Egmore Express and Train no. Guruvayur Trissur Passenger ")
-	window.alert("Location - Trissur Railway Station")
+	alert("Train no. 16128 Guruvayur Chennai Egmore Express and Train no. Guruvayur Trissur Passenger ")
+	alert("Location - Trissur Railway Station")
 }
 function OFF() {
-	window.alert("You're Offline")
+	alert("You're Offline")
 }
 function ON() {
-	window.alert("You're online")
+	alert("You're online")
 }
