79303a5e5b03b858c629f0a35d8a788a6708d316
Advaithva <97515865+Advaithva@users.noreply.github.com>
2022-04-05 08:49:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,9 @@ function PASS() {
 	window.alert("Train no. 16128 Guruvayur Chennai Egmore Express and Train no. Guruvayur Trissur Passenger ")
 	window.alert("Location - Trissur Railway Station")
 }
-	
+function OFF() {
+	window.alert("You're Offline")
+}
+function ON {
+	window.alert("You're online")
+}
