444f3f33b129827ed8aa5ea9ddf2845d73496073
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-10-09 09:24:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,14 +21,14 @@ function HMSFR() {
 	alert("Location - Banaswadi");
 }
 function BE() {
-	alert("Train no. 06525 Kanyakumari Bengaluru Island Express");
+	alert("Train no. 16525 Kanyakumari Bengaluru Island Express");
 	alert("Location - Bengaluru East");
 }
 function BANDSF() {
 	alert("Train no. 12684 Banaswadi Ernakulam Superfast Express")
 }
 function DEMU() {
-	alert("angalore Cantonment Kolar DEMU")
+	alert("Bangalore Cantonment Kolar DEMU")
 	alert("Location - Bangalore Cantonment")
 }
 function BNC() {
@@ -37,7 +37,7 @@ function BNC() {
 }
 
 function HSNSUR() {
-	alert("Train no. 11312 - Hassan Solapr Express")
+	alert("Train no. 11312 - Hassan Solapur Express")
 	alert("Location - Yeshvantpur Jn.")	
 }
 
