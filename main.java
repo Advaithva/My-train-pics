f8078e06a890455fca7c708e7002aaa9a10a2c6f
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-12-13 20:30:23 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,3 +1,11 @@
+function SUB() {
+  alert("You have clicked the submit Button. There is no going back");
+}
+
+function INVALID() {
+  alert("Invalid E-mail id. Check once again and re-enter");
+}
+
 // comment
 // how was it?
 // comment
